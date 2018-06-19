2573e3b4fffa9c025cbb08d5bb12d3d804856900
rafasystec <rafasystec@yahoo.com.br>
2018-06-18 22:53:41 -0300

    REST service for Professional and Client.

---
+++ genius-rest/src/main/java/br/com/barcadero/genius/rest/RESTProfessional.java
@@ -0,0 +1,118 @@
+package br.com.barcadero.genius.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.barcadero.genius.core.exceptions.ValidationException;
+import br.com.barcadero.genius.core.role.RoleProfessional;
+import br.com.barcadero.genius.persistence.model.Professional;
+import br.com.barcadero.genius.rest.util.RestUtil;
+
+
+
+
+@Component
+@Path("pro")
+public class RESTProfessional extends ASuperRestClass<Professional>{
+	@Autowired
+	private RoleProfessional roleProfessional;
+	
+	public RESTProfessional() {
+		
+	}
+	/**
+	 * Verifica se tal serviço REST está on-line
+	 * @return retorna OK se o servico ta no ar
+	 */
+	@GET
+	@Path("on")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response doGetOn(){
+		System.out.println("Server is OK!!!");
+		return Response.ok("OK").build();
+	}
+	
+	/**
+	 * <p>Obter um medico pelo seu ID</p>
+	 * <p>ex url: http://hostserver/rest/medico/1</p>
+	 * @param id do tipo long
+	 */
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response doGet(@PathParam("id") long id){
+		Professional entidade=null;
+		try {
+			entidade= roleProfessional.find(id);
+		} catch (ValidationException e) {
+			return RestUtil.getResponseValidationErro(e);
+		} catch (Exception e) {
+			return RestUtil.getResponseErroInesperado(e);
+		}
+
+		return Response.ok().entity(entidade).build();
+	}
+	/**
+	 * <p>Salva ou atualiza uma entidade de medico. Se a entidade medico vier com id ele atualiza caso contrario o 
+	 * mesmo é inserido como um novo registro</p>
+	 * <p>ex url: http://hostserver/rest/medico/save/{JSON do medico}</p>
+	 * @param professional JSON da entidade medico
+	 */
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response save(Professional professional){
+		if(professional.getId()>0){
+			try {
+				roleProfessional.update(professional);
+			} catch (ValidationException e) {
+				return RestUtil.getResponseValidationErro(e);
+			}catch (Exception e) {
+				return RestUtil.getResponseErroInesperado(e);
+			}
+		}else{
+			try {
+				roleProfessional.insert(professional);
+			} catch (ValidationException e) {
+				return RestUtil.getResponseValidationErro(e);
+			}catch (Exception e) {
+				return RestUtil.getResponseErroInesperado(e);
+			}
+		}
+		return Response.ok().entity(professional).build();
+	}
+	
+	
+	/**
+	 * <p>Deleta um medico pelo seu ID</p>
+	 * <p>ex url: http://hostserver/rest/medico/delete/1</p>
+	 * <p><strong>ATENÇÃO: Não poderá ser desfeita</strong></p>
+	 * @param id do medico a ser deletado
+	 */
+	@DELETE
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response delete(@PathParam("id") long id){
+		try {
+			roleProfessional.delete(id);
+		} catch (ValidationException e) {
+			return RestUtil.getResponseValidationErro(e);
+		}catch (Exception e) {
+			return RestUtil.getResponseErroInesperado(e);
+		}
+		return Response.ok().build();
+	}
+	
+
+
+}
